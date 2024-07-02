e4dd0a9e821776216b38e27416860da5d5e89031
Rahul Goswami <155542503+rahul-2396@users.noreply.github.com>
2024-07-02 17:47:06 +0530

    Time: 3 ms (90.95%), Space: 43 MB (58.37%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,31 +1,37 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return intersect(nums2, nums1);
+class Solution 
+{
+    public int[] intersect(int[] nums1, int[] nums2) 
+    {
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        if(length1 > length2)
+        {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
         }
-        
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums1) {
+        for(int num : nums1)
+        {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
-        List<Integer> result = new ArrayList<>();
-        for (int num : nums2) {
-            if (map.getOrDefault(num, 0) > 0) {
-                result.add(num);
-                map.put(num, map.get(num) - 1);
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums2)
+        {
+            if(map.containsKey(num) == true)
+            {
+                if(map.get(num) > 0)
+                {
+                    list.add(num);
+                    map.put(num, map.get(num) - 1);
+                }
             }
         }
-        
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++)
+        {
+            result[i] = list.get(i);
         }
-        return answer;
+        return result;
     }
 }
