4e0e0e3cb9ce90776f5120dd07347bc7e11e8c02
Rahul Goswami <155542503+rahul-2396@users.noreply.github.com>
2024-07-07 16:51:34 +0530

    Time: 103 ms (100%), Space: 40.4 MB (28.61%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,14 +1,15 @@
-class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
-        int drank = numBottles; // Initial number of full bottles you can drink
-        int emptyBottles = numBottles; // All initial bottles become empty after drinking
-
-        while (emptyBottles >= numExchange) {
-            int newBottles = emptyBottles / numExchange; // Number of new full bottles
-            drank += newBottles; // Add these to the total number of bottles drunk
-            emptyBottles = newBottles + (emptyBottles % numExchange); // Update empty bottles count
+class Solution 
+{
+    public int numWaterBottles(int numBottles, int numExchange) 
+    {
+        int drank = numBottles;
+        int emptyBottles = numBottles;
+        while(emptyBottles >= numExchange)
+        {
+            int newBottles = emptyBottles / numExchange;
+            drank += newBottles;
+            emptyBottles = newBottles + (emptyBottles % numExchange);
         }
-
         return drank;
     }
 }
