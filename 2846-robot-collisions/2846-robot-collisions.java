5ad0a169aac08922d79ef383ca907a5923f44e41
Rahul Goswami <155542503+rahul-2396@users.noreply.github.com>
2024-07-13 22:54:17 +0530

    Time: 38 ms (71.25%), Space: 57.7 MB (96.25%) - LeetHub

---
+++ 2846-robot-collisions/2846-robot-collisions.java
@@ -0,0 +1,53 @@
+class Solution 
+{
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions)
+    {
+        int length = positions.length;
+        Integer[] indices = new Integer[length];
+        List<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int index = 0; index < length; ++index)
+        {
+            indices[index] = index;
+        }
+        Arrays.sort(indices, (lhs, rhs) -> Integer.compare(positions[lhs], positions[rhs]));
+        for(int currentIndex : indices)
+        {
+            if(directions.charAt(currentIndex) == 'R')
+            {
+                stack.push(currentIndex);
+            }
+            else
+            {
+                while(!stack.isEmpty() && healths[currentIndex] > 0)
+                {
+                    int topIndex = stack.pop();
+                    if(healths[topIndex] > healths[currentIndex])
+                    {
+                        healths[topIndex] -= 1;
+                        healths[currentIndex] = 0;
+                        stack.push(topIndex);
+                    }
+                    else if(healths[topIndex] < healths[currentIndex])
+                    {
+                        healths[currentIndex] -= 1;
+                        healths[topIndex] = 0;
+                    }
+                    else
+                    {
+                        healths[currentIndex] = 0;
+                        healths[topIndex] = 0;
+                    }
+                }
+            }
+        }
+        for(int index = 0; index < length; ++index)
+        {
+            if(healths[index] > 0)
+            {
+                result.add(healths[index]);
+            }
+        }
+        return result;
+    }
+}
