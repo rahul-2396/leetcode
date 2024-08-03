e195328c6478ff65d6407c2b6858bb4419f6a9f6
Rahul Goswami <155542503+rahul-2396@users.noreply.github.com>
2024-08-03 17:48:27 +0530

    Time: 4 ms (54.32%), Space: 44.1 MB (56.26%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -8,17 +8,6 @@ class Solution
         }
         Arrays.sort(target);
         Arrays.sort(arr);
-        // for(int i = 0; i < target.length; i++)
-        // {
-        //     if(target[i] == arr[i])
-        //     {
-        //         continue;
-        //     }
-        //     else
-        //     {
-        //         return false;
-        //     }
-        // }
         if(Arrays.equals(target, arr))
         {
             return true;
