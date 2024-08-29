0da4c9d29893c013517f437ed38cb731c00db3fb
Rahul Goswami <155542503+rahul-2396@users.noreply.github.com>
2024-08-29 19:36:35 +0530

    Time: 32 ms (33.23%), Space: 44.7 MB (85.67%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -1,51 +1,43 @@
-class Solution {
-
-    public int removeStones(int[][] stones) {
+class Solution 
+{
+    public int removeStones(int[][] stones) 
+    {
         int n = stones.length;
-
-        // Adjacency list to store graph connections
         List<Integer>[] adjacencyList = new List[n];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++)
+        {
             adjacencyList[i] = new ArrayList<>();
         }
-
-        // Build the graph: Connect stones that share the same row or column
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (
-                    stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]
-                ) {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
+                {
                     adjacencyList[i].add(j);
                     adjacencyList[j].add(i);
                 }
             }
         }
-
-        int numOfConnectedComponents = 0;
+        int noOfConnectedComponents = 0;
         boolean[] visited = new boolean[n];
-
-        // Traverse all stones using DFS to count connected components
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
+        for(int i = 0; i < n; i++)
+        {
+            if(!visited[i])
+            {
                 depthFirstSearch(adjacencyList, visited, i);
-                numOfConnectedComponents++;
+                noOfConnectedComponents += 1;
             }
         }
-
-        // Maximum stones that can be removed is total stones minus number of connected components
-        return n - numOfConnectedComponents;
+        return n - noOfConnectedComponents;
     }
-
-    // DFS to visit all stones in a connected component
-    private void depthFirstSearch(
-        List<Integer>[] adjacencyList,
-        boolean[] visited,
-        int stone
-    ) {
+    private void depthFirstSearch(List<Integer>[] adjacencyList, boolean[] visited, int stone)
+    {
         visited[stone] = true;
-
-        for (int neighbor : adjacencyList[stone]) {
-            if (!visited[neighbor]) {
+        for(int neighbor : adjacencyList[stone])
+        {
+            if(!visited[neighbor])
+            {
                 depthFirstSearch(adjacencyList, visited, neighbor);
             }
         }
