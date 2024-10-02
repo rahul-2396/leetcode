eb26bcdd05150db9b66b78c233c382337d4f2b9b
Rahul Goswami <155542503+rahul-2396@users.noreply.github.com>
2024-10-02 16:01:07 +0530

    Time: 32 ms (41.74%), Space: 62.7 MB (16.45%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -2,15 +2,15 @@ class Solution
 {
     public int[] arrayRankTransform(int[] arr) 
     {
-        if(arr.length == 0)
-        {
-            return new int[0];
-        }
+        if(arr.length == 0) return new int[0];
+
         int[] newArr = new int[arr.length];
         System.arraycopy(arr, 0, newArr, 0, arr.length);
         Arrays.sort(newArr);
+
         Map<Integer, Integer> map = new HashMap<>();
         int rank = 1;
+
         for(int num : newArr)
         {
             if(!map.containsKey(num))
@@ -19,11 +19,13 @@ class Solution
                 rank += 1;
             }
         }
+
         int[] result = new int[arr.length];
         for(int i = 0; i < arr.length; i++)
         {
             result[i] = map.get(arr[i]);
         }
+
         return result;
     }
 }
